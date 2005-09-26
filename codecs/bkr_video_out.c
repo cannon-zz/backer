5bf0b53fd36ea9c4140fc933cd64c76d5a4f05fa
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2005-09-26 01:40:41 +0000

    Revert plugin template to GStreamer 0.8 (I had inadvertently grabbed the 0.9 template, and it wouldn't build).
    git-svn-id: file:///var/lib/svn/backer/trunk@68 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- codecs/bkr_video_out.c
+++ codecs/bkr_video_out.c
@@ -91,21 +91,27 @@ static void	gst_plugin_template_update_plugin(const GValue *value,
 static void	gst_plugin_template_update_mute	(const GValue *value,
 						 gpointer data);
 
-static GstFlowReturn gst_plugin_template_chain	(GstPad *pad, GstBuffer *buf);
+static void	gst_plugin_template_chain	(GstPad *pad, GstData *in);
 
 static GstElementClass *parent_class = NULL;
 
 /* this function handles the link with other plug-ins */
-static gboolean
-gst_plugin_template_set_caps (GstPad *pad, GstCaps *caps)
+static GstPadLinkReturn
+gst_plugin_template_link (GstPad *pad, const GstCaps *caps)
 {
   GstPluginTemplate *filter;
   GstPad *otherpad;
 
   filter = GST_PLUGIN_TEMPLATE (gst_pad_get_parent (pad));
-  otherpad = (pad == filter->srcpad) ? filter->sinkpad : filter->srcpad;
+  g_return_val_if_fail (filter != NULL, GST_PAD_LINK_REFUSED);
+  g_return_val_if_fail (GST_IS_PLUGIN_TEMPLATE (filter),
+                        GST_PAD_LINK_REFUSED);
+  otherpad = (pad == filter->srcpad ? filter->sinkpad : filter->srcpad);
 
-  return gst_pad_set_caps (pad, caps);
+  /* set caps on next or previous element's pad, and see what they
+   * think. In real cases, we would (after this step) extract
+   * properties from the caps such as video size or audio samplerat. */
+  return gst_pad_try_set_caps (otherpad, caps);
 }
 
 GType
@@ -184,11 +190,12 @@ gst_plugin_template_init (GstPluginTemplate *filter)
 
   filter->sinkpad = gst_pad_new_from_template (
 	gst_element_class_get_pad_template (klass, "sink"), "sink");
-  gst_pad_set_setcaps_function (filter->sinkpad, gst_plugin_template_set_caps);
+  gst_pad_set_link_function (filter->sinkpad, gst_plugin_template_link);
   gst_pad_set_getcaps_function (filter->sinkpad, gst_pad_proxy_getcaps);
 
   filter->srcpad = gst_pad_new_from_template (
 	gst_element_class_get_pad_template (klass, "src"), "src");
+  gst_pad_set_link_function (filter->srcpad, gst_plugin_template_link);
   gst_pad_set_getcaps_function (filter->srcpad, gst_pad_proxy_getcaps);
 
   gst_element_add_pad (GST_ELEMENT (filter), filter->sinkpad);
@@ -201,10 +208,13 @@ gst_plugin_template_init (GstPluginTemplate *filter)
  * this function does the actual processing
  */
 
-static GstFlowReturn
-gst_plugin_template_chain (GstPad *pad, GstBuffer *buf)
+static void
+gst_plugin_template_chain (GstPad *pad, GstData *in)
 {
   GstPluginTemplate *filter;
+  GstBuffer *out_buf, *buf = GST_BUFFER (in);
+  gfloat *data;
+  gint i, num_samples;
 
   g_return_if_fail (GST_IS_PAD (pad));
   g_return_if_fail (buf != NULL);
@@ -216,7 +226,7 @@ gst_plugin_template_chain (GstPad *pad, GstBuffer *buf)
     g_print ("I'm plugged, therefore I'm in.\n");
 
   /* just push out the incoming buffer without touching it */
-  return gst_pad_push (filter->srcpad, buf);
+  gst_pad_push (filter->srcpad, GST_DATA (buf));
 }
 
 static void

8dd2368a50a58331faa8866b74bd994c2578c9cc
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2005-09-26 02:48:43 +0000

    - Correct the name space. - Move things around so function prototypes aren't needed.
    git-svn-id: file:///var/lib/svn/backer/trunk@69 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- codecs/bkr_video_out.c
+++ codecs/bkr_video_out.c
@@ -50,247 +50,206 @@
 
 /* Filter signals and args */
 enum {
-  /* FILL ME */
-  LAST_SIGNAL
+	/* FILL ME */
+	LAST_SIGNAL
 };
 
 enum {
-  ARG_0,
-  ARG_SILENT
+	ARG_0,
+	ARG_SILENT
 };
 
-static GstStaticPadTemplate sink_factory =
-GST_STATIC_PAD_TEMPLATE (
-  "sink",
-  GST_PAD_SINK,
-  GST_PAD_ALWAYS,
-  GST_STATIC_CAPS ("ANY")
-);
+static GstStaticPadTemplate sink_factory = GST_STATIC_PAD_TEMPLATE("sink", GST_PAD_SINK, GST_PAD_ALWAYS, GST_STATIC_CAPS("ANY"));
 
-static GstStaticPadTemplate src_factory =
-GST_STATIC_PAD_TEMPLATE (
-  "src",
-  GST_PAD_SRC,
-  GST_PAD_ALWAYS,
-  GST_STATIC_CAPS ("ANY")
-);
-
-static void	gst_plugin_template_class_init	(GstPluginTemplateClass *klass);
-static void	gst_plugin_template_base_init	(GstPluginTemplateClass *klass);
-static void	gst_plugin_template_init	(GstPluginTemplate *filter);
-
-static void	gst_plugin_template_set_property(GObject *object, guint prop_id,
-                                                 const GValue *value,
-					         GParamSpec *pspec);
-static void	gst_plugin_template_get_property(GObject *object, guint prop_id,
-                                                 GValue *value,
-						 GParamSpec *pspec);
-
-static void	gst_plugin_template_update_plugin(const GValue *value,
-						  gpointer data);
-static void	gst_plugin_template_update_mute	(const GValue *value,
-						 gpointer data);
-
-static void	gst_plugin_template_chain	(GstPad *pad, GstData *in);
+static GstStaticPadTemplate src_factory = GST_STATIC_PAD_TEMPLATE("src", GST_PAD_SRC, GST_PAD_ALWAYS, GST_STATIC_CAPS("ANY"));
 
 static GstElementClass *parent_class = NULL;
 
-/* this function handles the link with other plug-ins */
-static GstPadLinkReturn
-gst_plugin_template_link (GstPad *pad, const GstCaps *caps)
+
+/*
+ * this function handles the link with other plug-ins
+ */
+
+static GstPadLinkReturn bkr_video_out_link(GstPad *pad, const GstCaps *caps)
 {
-  GstPluginTemplate *filter;
-  GstPad *otherpad;
+	BkrVideoOut *filter;
+	GstPad *otherpad;
 
-  filter = GST_PLUGIN_TEMPLATE (gst_pad_get_parent (pad));
-  g_return_val_if_fail (filter != NULL, GST_PAD_LINK_REFUSED);
-  g_return_val_if_fail (GST_IS_PLUGIN_TEMPLATE (filter),
-                        GST_PAD_LINK_REFUSED);
-  otherpad = (pad == filter->srcpad ? filter->sinkpad : filter->srcpad);
+	filter = BKR_VIDEO_OUT(gst_pad_get_parent(pad));
+	g_return_val_if_fail(filter != NULL, GST_PAD_LINK_REFUSED);
+	g_return_val_if_fail(GST_IS_BKR_VIDEO_OUT(filter), GST_PAD_LINK_REFUSED);
+	otherpad = (pad == filter->srcpad ? filter->sinkpad : filter->srcpad);
 
-  /* set caps on next or previous element's pad, and see what they
-   * think. In real cases, we would (after this step) extract
-   * properties from the caps such as video size or audio samplerat. */
-  return gst_pad_try_set_caps (otherpad, caps);
+	/* set caps on next or previous element's pad, and see what they
+	 * think. In real cases, we would (after this step) extract
+	 * properties from the caps such as video size or audio samplerat. */
+	return gst_pad_try_set_caps(otherpad, caps);
 }
 
-GType
-gst_gst_plugin_template_get_type (void)
-{
-  static GType plugin_type = 0;
 
-  if (!plugin_type)
-  {
-    static const GTypeInfo plugin_info =
-    {
-      sizeof (GstPluginTemplateClass),
-      (GBaseInitFunc) gst_plugin_template_base_init,
-      NULL,
-      (GClassInitFunc) gst_plugin_template_class_init,
-      NULL,
-      NULL,
-      sizeof (GstPluginTemplate),
-      0,
-      (GInstanceInitFunc) gst_plugin_template_init,
-    };
-    plugin_type = g_type_register_static (GST_TYPE_ELEMENT,
-	                                  "GstPluginTemplate",
-	                                  &plugin_info, 0);
-  }
-  return plugin_type;
+static void bkr_video_out_base_init(BkrVideoOutClass *klass)
+{
+	GstElementClass *element_class = GST_ELEMENT_CLASS(klass);
+	static GstElementDetails plugin_details = {
+		"Backer Video Out",
+		"Codec/Decoder/Video/bkr_video_out",
+		"Simulates a Backer's byte-stream to video conversion",
+		"Kipp Cannon <kipp@gravity.phys.uwm.edu>"
+	};
+
+	gst_element_class_add_pad_template(element_class, gst_static_pad_template_get(&src_factory));
+	gst_element_class_add_pad_template(element_class, gst_static_pad_template_get(&sink_factory));
+	gst_element_class_set_details(element_class, &plugin_details);
 }
 
-static void
-gst_plugin_template_base_init (GstPluginTemplateClass *klass)
-{
-  static GstElementDetails plugin_details = {
-    "PluginTemplate",
-    "Generic/PluginTemplate",
-    "Generic Template Plugin",
-    "Thomas Vander Stichele <thomas@apestaart.org>"
-  };
-  GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
 
-  gst_element_class_add_pad_template (element_class,
-	gst_static_pad_template_get (&src_factory));
-  gst_element_class_add_pad_template (element_class,
-	gst_static_pad_template_get (&sink_factory));
-  gst_element_class_set_details (element_class, &plugin_details);
+/*
+ * chain function
+ * this function does the actual processing
+ */
+
+static void bkr_video_out_chain(GstPad *pad, GstData *in)
+{
+	BkrVideoOut *filter;
+	GstBuffer *out_buf, *buf = GST_BUFFER(in);
+	gfloat *data;
+	gint i, num_samples;
+
+	g_return_if_fail(GST_IS_PAD(pad));
+	g_return_if_fail(buf != NULL);
+
+	filter = BKR_VIDEO_OUT(GST_OBJECT_PARENT(pad));
+	g_return_if_fail(GST_IS_BKR_VIDEO_OUT(filter));
+
+	if (filter->silent == FALSE)
+		g_print("I'm plugged, therefore I'm in.\n");
+
+	/* just push out the incoming buffer without touching it */
+	gst_pad_push(filter->srcpad, GST_DATA(buf));
 }
 
-/* initialize the plugin's class */
-static void
-gst_plugin_template_class_init (GstPluginTemplateClass *klass)
-{
-  GObjectClass *gobject_class;
-  GstElementClass *gstelement_class;
 
-  gobject_class = (GObjectClass*) klass;
-  gstelement_class = (GstElementClass*) klass;
-
-  parent_class = g_type_class_ref (GST_TYPE_ELEMENT);
-
-  g_object_class_install_property (gobject_class, ARG_SILENT,
-    g_param_spec_boolean ("silent", "Silent", "Produce verbose output ?",
-                          FALSE, G_PARAM_READWRITE));
-
-  gobject_class->set_property = gst_plugin_template_set_property;
-  gobject_class->get_property = gst_plugin_template_get_property;
-}
-
-/* initialize the new element
+/*
+ * initialize the new element
  * instantiate pads and add them to element
  * set functions
  * initialize structure
  */
-static void
-gst_plugin_template_init (GstPluginTemplate *filter)
+
+static void bkr_video_out_init(BkrVideoOut *filter)
 {
-  GstElementClass *klass = GST_ELEMENT_GET_CLASS (filter);
+	GstElementClass *klass = GST_ELEMENT_GET_CLASS(filter);
 
-  filter->sinkpad = gst_pad_new_from_template (
-	gst_element_class_get_pad_template (klass, "sink"), "sink");
-  gst_pad_set_link_function (filter->sinkpad, gst_plugin_template_link);
-  gst_pad_set_getcaps_function (filter->sinkpad, gst_pad_proxy_getcaps);
+	filter->sinkpad = gst_pad_new_from_template(gst_element_class_get_pad_template(klass, "sink"), "sink");
+	gst_pad_set_link_function(filter->sinkpad, bkr_video_out_link);
+	gst_pad_set_getcaps_function(filter->sinkpad, gst_pad_proxy_getcaps);
 
-  filter->srcpad = gst_pad_new_from_template (
-	gst_element_class_get_pad_template (klass, "src"), "src");
-  gst_pad_set_link_function (filter->srcpad, gst_plugin_template_link);
-  gst_pad_set_getcaps_function (filter->srcpad, gst_pad_proxy_getcaps);
+	filter->srcpad = gst_pad_new_from_template(gst_element_class_get_pad_template(klass, "src"), "src");
+	gst_pad_set_link_function(filter->srcpad, bkr_video_out_link);
+	gst_pad_set_getcaps_function(filter->srcpad, gst_pad_proxy_getcaps);
 
-  gst_element_add_pad (GST_ELEMENT (filter), filter->sinkpad);
-  gst_element_add_pad (GST_ELEMENT (filter), filter->srcpad);
-  gst_pad_set_chain_function (filter->sinkpad, gst_plugin_template_chain);
-  filter->silent = FALSE;
+	gst_element_add_pad(GST_ELEMENT(filter), filter->sinkpad);
+	gst_element_add_pad(GST_ELEMENT(filter), filter->srcpad);
+	gst_pad_set_chain_function(filter->sinkpad, bkr_video_out_chain);
+	filter->silent = FALSE;
 }
 
-/* chain function
- * this function does the actual processing
+
+static void bkr_video_out_set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec)
+{
+	BkrVideoOut *filter;
+
+	g_return_if_fail(GST_IS_BKR_VIDEO_OUT(object));
+	filter = BKR_VIDEO_OUT(object);
+
+	switch (prop_id) {
+	case ARG_SILENT:
+		filter->silent = g_value_get_boolean(value);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+		break;
+	}
+}
+
+
+static void bkr_video_out_get_property(GObject *object, guint prop_id, GValue *value, GParamSpec *pspec)
+{
+	BkrVideoOut *filter;
+
+	g_return_if_fail(GST_IS_BKR_VIDEO_OUT(object));
+	filter = BKR_VIDEO_OUT(object);
+
+	switch (prop_id) {
+	case ARG_SILENT:
+		g_value_set_boolean(value, filter->silent);
+		break;
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID(object, prop_id, pspec);
+		break;
+	}
+}
+
+
+/*
+ * Initialize the plugin's class
  */
 
-static void
-gst_plugin_template_chain (GstPad *pad, GstData *in)
+static void bkr_video_out_class_init(BkrVideoOutClass *klass)
 {
-  GstPluginTemplate *filter;
-  GstBuffer *out_buf, *buf = GST_BUFFER (in);
-  gfloat *data;
-  gint i, num_samples;
+	GObjectClass *gobject_class;
+	GstElementClass *gstelement_class;
 
-  g_return_if_fail (GST_IS_PAD (pad));
-  g_return_if_fail (buf != NULL);
+	gobject_class = (GObjectClass *) klass;
+	gstelement_class = (GstElementClass *) klass;
 
-  filter = GST_PLUGIN_TEMPLATE (GST_OBJECT_PARENT (pad));
-  g_return_if_fail (GST_IS_PLUGIN_TEMPLATE (filter));
+	parent_class = g_type_class_ref(GST_TYPE_ELEMENT);
 
-  if (filter->silent == FALSE)
-    g_print ("I'm plugged, therefore I'm in.\n");
+	g_object_class_install_property(gobject_class, ARG_SILENT, g_param_spec_boolean("silent", "Silent", "Produce verbose output ?", FALSE, G_PARAM_READWRITE));
 
-  /* just push out the incoming buffer without touching it */
-  gst_pad_push (filter->srcpad, GST_DATA (buf));
+	gobject_class->set_property = bkr_video_out_set_property;
+	gobject_class->get_property = bkr_video_out_get_property;
 }
 
-static void
-gst_plugin_template_set_property (GObject *object, guint prop_id,
-                                  const GValue *value, GParamSpec *pspec)
+
+GType bkr_video_out_get_type(void)
 {
-  GstPluginTemplate *filter;
+	static GType plugin_type = 0;
 
-  g_return_if_fail (GST_IS_PLUGIN_TEMPLATE (object));
-  filter = GST_PLUGIN_TEMPLATE (object);
-
-  switch (prop_id)
-  {
-  case ARG_SILENT:
-    filter->silent = g_value_get_boolean (value);
-    break;
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    break;
-  }
+	if (!plugin_type) {
+		static const GTypeInfo plugin_info = {
+			sizeof(BkrVideoOutClass),
+			(GBaseInitFunc) bkr_video_out_base_init,
+			NULL,
+			(GClassInitFunc) bkr_video_out_class_init,
+			NULL,
+			NULL,
+			sizeof(BkrVideoOut),
+			0,
+			(GInstanceInitFunc) bkr_video_out_init,
+		};
+		plugin_type = g_type_register_static(GST_TYPE_ELEMENT, "BkrVideoOut", &plugin_info, 0);
+	}
+	return plugin_type;
 }
 
-static void
-gst_plugin_template_get_property (GObject *object, guint prop_id,
-                                  GValue *value, GParamSpec *pspec)
-{
-  GstPluginTemplate *filter;
 
-  g_return_if_fail (GST_IS_PLUGIN_TEMPLATE (object));
-  filter = GST_PLUGIN_TEMPLATE (object);
-
-  switch (prop_id) {
-  case ARG_SILENT:
-    g_value_set_boolean (value, filter->silent);
-    break;
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    break;
-  }
-}
-
-/* entry point to initialize the plug-in
+/*
+ * entry point to initialize the plug-in
  * initialize the plug-in itself
  * register the element factories and pad templates
  * register the features
  */
-static gboolean
-plugin_init (GstPlugin *plugin)
+
+static gboolean plugin_init(GstPlugin *plugin)
 {
-  return gst_element_register (plugin, "plugin",
-			       GST_RANK_NONE,
-			       GST_TYPE_PLUGIN_TEMPLATE);
+	return gst_element_register(plugin, "plugin", GST_RANK_NONE, BKR_VIDEO_OUT_TYPE);
 }
 
-/* this is the structure that gst-register looks for
- * so keep the name plugin_desc, or you cannot get your plug-in registered */
-GST_PLUGIN_DEFINE (
-  GST_VERSION_MAJOR,
-  GST_VERSION_MINOR,
-  "plugin",
-  "Template plugin",
-  plugin_init,
-  VERSION,
-  "LGPL",
-  "GStreamer",
-  "http://gstreamer.net/"
-)
+
+/*
+ * this is the structure that gst-register looks for
+ * so keep the name plugin_desc, or you cannot get your plug-in registered
+ */
+
+GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, "plugin", "Template plugin", plugin_init, VERSION, "LGPL", "GStreamer", "http://gstreamer.net/")

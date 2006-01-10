356db4c3f8e9693af20fb0cc1dc9ea3616bd3cd7
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2006-01-10 08:24:32 +0000

    Convert some #defines to enums.
    git-svn-id: file:///var/lib/svn/backer/trunk@80 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- drivers/backer.h
+++ drivers/backer.h
@@ -173,13 +173,19 @@
 #define  BKR_VIDEOMODE(x)      ((x) & BKR_VIDEOMODE_MASK)
 #define  BKR_DENSITY(x)        ((x) & BKR_DENSITY_MASK)
 #define  BKR_CODEC(x)          ((x) & BKR_CODEC_MASK)
-#define  BKR_NTSC              0x0001           /* NTSC video mode */
-#define  BKR_PAL               0x0002           /* PAL video mode */
-#define  BKR_LOW               0x0004           /* Low density */
-#define  BKR_HIGH              0x0008           /* High density */
-#define  BKR_RAW               0x0000           /* read/write raw data */
-#define  BKR_SP                0x0010           /* read/write SP/LP format */
-#define  BKR_EP                0x0020           /* read/write EP format */
+enum bkr_vidmodes {
+	BKR_NTSC = 0x0001,     /* NTSC video mode */
+	BKR_PAL = 0x0002       /* PAL video mode */
+};
+enum bkr_densities {
+	BKR_LOW = 0x0004,      /* Low density */
+	BKR_HIGH = 0x0008      /* High density */
+};
+enum bkr_formats {
+	BKR_RAW = 0x0000,      /* read/write raw data */
+	BKR_SP = 0x0010,       /* read/write SP/LP format */
+	BKR_EP = 0x0020        /* read/write EP format */
+};
 
 
 /*

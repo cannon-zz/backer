bb9b2a87c7eb1b60c7645a6b31c9d7b54bbbfb0a
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2008-07-21 06:03:09 +0000

    whitespace.
    git-svn-id: file:///var/lib/svn/backer/trunk@585 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- codecs/bkr_elements.h
+++ codecs/bkr_elements.h
@@ -20,24 +20,33 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+
 #ifndef __BKR_ELEMENTS_H__
 #define __BKR_ELEMENTS_H__
 
+
 #include <gst/gst.h>
 
+
 G_BEGIN_DECLS
+
 
 #define BKR_TYPE_VIDEOMODE	(bkr_videomode_get_type())
 #define BKR_TYPE_BITDENSITY	(bkr_bitdensity_get_type())
 #define BKR_TYPE_SECTORFORMAT	(bkr_sectorformat_get_type())
 
+
 GType bkr_videomode_get_type(void);
 GType bkr_bitdensity_get_type(void);
 GType bkr_sectorformat_get_type(void);
+
 
 #define DEFAULT_VIDEOMODE	BKR_NTSC
 #define DEFAULT_BITDENSITY	BKR_HIGH
 #define DEFAULT_SECTORFORMAT	BKR_SP
 
+
 G_END_DECLS
+
+
 #endif	/* __BKR_ELEMENTS_H__ */

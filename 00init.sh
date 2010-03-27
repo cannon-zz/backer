d29f888367f89efbc0eb4a09744e9f88a5cd9794
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2010-03-27 05:22:38 +0000

    fix bashism
    git-svn-id: file:///var/lib/svn/backer/trunk@845 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- 00init.sh
+++ 00init.sh
@@ -2,7 +2,7 @@
 
 # Check with the user
 echo "This script (re-)builds the source tree's configuration and build system."
-read -p "Press CTRL-C to abort, or RETURN to continue... "
+read -p "Press CTRL-C to abort, or RETURN to continue... " RESPONSE
 
 # Get it done
 {

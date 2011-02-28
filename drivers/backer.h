649b9d57a8614b31647e7840cdb0270b3517b732
kipp <kipp@4b75c72c-9afd-0310-a208-b187eb4cea4a>
2011-02-28 01:35:36 +0000

    remove unused modulation_pad element from bkr_format_info_t structure
    git-svn-id: file:///var/lib/svn/backer/trunk@1040 4b75c72c-9afd-0310-a208-b187eb4cea4a

--- drivers/backer.h
+++ drivers/backer.h
@@ -238,23 +238,22 @@ typedef struct {
 	unsigned int  field_size;       /* bytes in an even video field */
 	unsigned int  interlace;        /* difference between odd/even fields */
 	unsigned int  frame_size;       /* bytes in a full video frame */
-	unsigned int  modulation_pad;   /* modulation overhead */
 } bkr_format_info_t;
 
 
 #define BKR_FORMAT_INFO_INITIALIZER   (bkr_format_info_t []) \
-{ {  4, 1012,  4, 2028, 102 },     /* nle */ \
-  {  4, 1012,  4, 2028,   0 },     /* nlr */ \
-  {  4, 1012,  4, 2028,   0 },     /* nls */ \
-  {  4, 1220,  0, 2440, 123 },     /* ple */ \
-  {  4, 1220,  0, 2440,   0 },     /* plr */ \
-  {  4, 1220,  0, 2440,   0 },     /* pls */ \
-  { 10, 2530, 10, 5070, 259 },     /* nhe */ \
-  { 10, 2530, 10, 5070,   0 },     /* nhr */ \
-  { 10, 2530, 10, 5070,   0 },     /* nhs */ \
-  { 10, 3050,  0, 6100, 312 },     /* phe */ \
-  { 10, 3050,  0, 6100,   0 },     /* phr */ \
-  { 10, 3050,  0, 6100,   0 } }    /* phs */
+{ {  4, 1012,  4, 2028 },     /* nle */ \
+  {  4, 1012,  4, 2028 },     /* nlr */ \
+  {  4, 1012,  4, 2028 },     /* nls */ \
+  {  4, 1220,  0, 2440 },     /* ple */ \
+  {  4, 1220,  0, 2440 },     /* plr */ \
+  {  4, 1220,  0, 2440 },     /* pls */ \
+  { 10, 2530, 10, 5070 },     /* nhe */ \
+  { 10, 2530, 10, 5070 },     /* nhr */ \
+  { 10, 2530, 10, 5070 },     /* nhs */ \
+  { 10, 3050,  0, 6100 },     /* phe */ \
+  { 10, 3050,  0, 6100 },     /* phr */ \
+  { 10, 3050,  0, 6100 } }    /* phs */
 
 
 #define BKR_NUM_FORMATS (sizeof(BKR_FORMAT_INFO_INITIALIZER)/sizeof(bkr_format_info_t))
